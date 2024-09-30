b9a1a398866f1dcad92a9eec17fc93b02bc9a791
blakedoescoding <blakedoescoding@gmail.com>
2024-09-29 23:36:02 -0500

    Added my solutions for array exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //splits string into array of chars
+    //reverses the array
+    //join the chars back into string
+    return string.split('').reverse().join(''); 
 
+
+
+    //Another way, starts with empty string,
+    //starts at last letter and goes backwards
+    // let answer = '';
+    // for (let i = string.at(-1); i >= 0; i--){
+    //     answer += string[i];
+    // }
+    // return answer;
+    
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...number) {
+    const answer = [];
+    //loop through array
+    for (let i = 0; i < array.length; i++){
+        //if array DOES NOT EQUAL the number
+        //then push those numbers into a new array
+        //so it will not include the number 
+        if (array[i] !== number){
+            answer.push(array[i]);
+        }
+    }
+
+
+    return answer;
 };
 
 // Do not edit below this line
